a4655620edde50125c94e882fb9f9ad9afadfc14
Tom Maneri <tommaneri@gmail.com>
2022-03-29 22:36:26 -0700

    dedup

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ const packageName = process.env.npm_package_name;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    dedupe: ["vue"],
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
